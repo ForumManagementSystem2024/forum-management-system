7cd655911553d37731a9d68309a66d2c3a414873
Nikolay Voynov <nikolay_voynov@abv.bg>
2024-10-17 11:04:33 +0300

    project init

---
+++ forum-management-system/src/test/java/com/forum/forummanagementsystem/ForumManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forum.forummanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
