d7e2d31ec7b50a50bb3d05bac9973f07b84e8d0b
Nikolay Voynov <nikolay_voynov@abv.bg>
2024-10-24 13:20:42 +0300

    update ReplyService

--- forum-management-system/src/main/java/com/forum/forummanagementsystem/services/interfaces/ReplyService.java
+++ forum-management-system/src/main/java/com/forum/forummanagementsystem/services/interfaces/ReplyService.java
@@ -8,7 +8,6 @@ import com.forum.forummanagementsystem.models.User;
 import java.util.List;
 
 public interface ReplyService {
-    List<Reply> getReplies(FilterOptions filterOptions);
     Reply getReplyById(int id);
     void createReply(Post post, User user, Reply reply);
     void updateReply(User user, Reply reply);
