928be90ed2cc4bfb89173215e670035dc6184599
annavulcheva <anna.valcheva26@gmail.com>
2024-11-12 18:52:38 +0200

    implemented about view

--- forum-management-system/src/main/java/com/forum/forummanagementsystem/controllers/mvc/HomeMvcController.java
+++ forum-management-system/src/main/java/com/forum/forummanagementsystem/controllers/mvc/HomeMvcController.java
@@ -12,4 +12,9 @@ public class HomeMvcController {
     public String showHomePage() {
         return "index";
     }
+
+    @GetMapping("/about")
+    public String showAboutPage() {
+        return "about";
+    }
 }
